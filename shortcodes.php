3d183e6760c9a2bd5c81d8e3ef56a483e1fc9716
David Balyan <davidbalyan9@gmail.com>
2025-06-09 13:15:58 +0300

    added styles to review btn

--- shortcodes.php
+++ shortcodes.php
@@ -108,7 +108,7 @@ function casinos_shortcode($atts) {
         $output .= '</td>';
 
         $output .= '<td>';
-        $output .= '<a href="' . esc_url($official_site) . '" class="btn btn-dark d-inline-flex align-items-center" target="_blank"><span style="margin-right:0.4em; display:inline-block;">'
+        $output .= '<a href="' . esc_url($official_site) . '" class="btn btn-dark d-inline-flex align-items-center" target="_blank"><span style="display:flex;justify-content:center;align-items:center;margin-right:.4rem;">'
             . '<svg width="1em" height="1em" viewBox="0 0 24 24" fill="currentColor" xmlns="http://www.w3.org/2000/svg"><path d="M12 17.27L18.18 21l-1.64-7.03L22 9.24l-7.19-.61L12 2 9.19 8.63 2 9.24l5.46 4.73L5.82 21z"/></svg>'
             . '</span>Review</a>';
         $output .= '</td>';
